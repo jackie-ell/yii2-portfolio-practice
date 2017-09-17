7c9c6beb6c9044792e0370acaf50dd11ac07c430
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-09-17 10:58:08 -0700

    - Add profile to about me page - Add technology labels to projects - Add color transition to social media links

--- views/site/about.php
+++ views/site/about.php
@@ -9,6 +9,10 @@ $this->title = 'About me';
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <p>
+      A web developer who lives for those “Eureka!” moments. When I’m not coding I’m playing table-top and board games.
+    </p>
+
     <h3>Technical Skills</h3>
     <p><strong>Languages:</strong> Ruby, HTML5 &amp; CSS3, JavaScript, AJAX, JSON, PHP</p>
     <p><strong>Frameworks:</strong> Rails, Node.js, jQuery, CodeIgniter, Bootstrap<p>
@@ -19,13 +23,13 @@ $this->title = 'About me';
 
     <table class="table">
       <tr>
-        <td><strong>Web Development Certificate</strong></td><td>Vancouver</td>
+        <th>Web Development Certificate</th><td>Vancouver</td>
       </tr>
       <tr>
         <td>CodeCore Developer Bootcamp</td><td>June - Aug 2017</td>
       </tr>
       <tr>
-        <td><strong>Computer Science Diploma</strong></td><td>New Westminster</td>
+        <th>Computer Science Diploma</th><td>New Westminster</td>
       </tr>
       <tr>
         <td>Douglas College</td><td>Graduated Feb 2017</td>
--- views/site/index.php
+++ views/site/index.php
@@ -22,14 +22,18 @@ $this->title = 'Jackie Ellenberger';
 
 
     <div class="body-content">
-
         <div class="row">
             <div class="col-xs-12 col-md-4">
               <div class="thumbnail">
                 <img src="<?php echo Yii::getAlias('@web').'/assets/images/code-drillz.png' ?>" alt="natural disaster img" class="img-thumbnail project"/>
                 <div class="caption">
+                  <span class="label label-primary">Ruby on Rails</span>
+                  <span class="label label-primary">PostgreSQL</span>
+                  <span class="label label-primary">Bootstrap</span>
+                  <span class="label label-primary">HTML5 &amp; CSS3</span>
+                  <span class="label label-primary">JavaScript</span>
                   <h3>Code Drillz</h3>
-                  <p>A quizzing site made to test users in various tech-related topics. This was a collaborative project created with Ruby on Rails, Bootstrap, HTML, CSS and JavaScript. My role involved creating and managing Rails controllers, and preparing views for the front-end team to style.</p>
+                  <p>A quizzing site made to test users in various tech-related topics. This was a collaborative project created with Ruby on Rails, Bootstrap, HTML5, CSS3 and JavaScript. My role involved creating and managing Rails controllers, and preparing views for the front-end team to style.</p>
                   <p>
                     <a class="btn btn-primary" href="https://railsonfire.herokuapp.com/">View &raquo;</a>
                     <a class="btn btn-default" href="https://github.com/jackie-ell/ROF">Source &raquo;</a>
@@ -41,6 +45,9 @@ $this->title = 'Jackie Ellenberger';
               <div class="thumbnail">
                 <img src="https://raw.githubusercontent.com/jackie-ell/natural-disasters/master/public/images/Screen%20Shot%202017-09-13%20at%201.11.04%20PM.png" alt="natural disaster img" class="img-thumbnail project"/>
                 <div class="caption">
+                  <span class="label label-primary">Node.js</span>
+                  <span class="label label-primary">Express.js</span>
+                  <span class="label label-primary">MongoDB</span>
                   <h3>Natural Disaster API</h3>
                   <p>An API that allows developers to quickly fetch thousands of rows of data on various natural disasters. The data is formatted in GeoJSON which allows it to be imported into the Google Maps API. This was a personal project created with Node.js, Express.js, and MongoDB, with a heavy emphasis on data scrapers.</p>
                   <p><a class="btn btn-default" href="https://github.com/jackie-ell/natural-disasters">Source &raquo;</a></p>
@@ -51,8 +58,11 @@ $this->title = 'Jackie Ellenberger';
               <div class="thumbnail">
                 <img src="https://raw.githubusercontent.com/jackie-ell/natural-disasters-spa/master/public/Screen%20Shot%202017-09-07%20at%2012.08.15%20PM.png" alt="natural disaster spa img" class="img-thumbnail project"/>
                 <div class="caption">
+                  <span class="label label-primary">HTML5 &amp; CSS3</span>
+                  <span class="label label-primary">JavaScript</span>
+                  <span class="label label-primary">Google Maps API</span>
                   <h3>Natural Disaster SPA</h3>
-                  <p>Single page frontend application to show off my natural disasters API. This project was built with HTML5, CSS3, and JavaScript.</p>
+                  <p>Single page front-end application to demonstrate my Natural Disasters API by importing the data into the Google Maps API. This project was built with HTML5, CSS3, and JavaScript.</p>
                   <p><a class="btn btn-default" href="https://github.com/jackie-ell/natural-disasters">Source &raquo;</a></p>
                 </div>
               </div>
